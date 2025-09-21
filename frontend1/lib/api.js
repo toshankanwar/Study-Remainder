37de408465552a0087eb03756ced990466f5a676
David_one8 <davidfule18@gmail.com>
2025-09-21 22:14:26 +0530

    Fix template literal syntax for Authorization header

--- frontend1/lib/api.js
+++ frontend1/lib/api.js
@@ -15,7 +15,7 @@ api.interceptors.request.use(
     if (typeof window !== 'undefined') {
       const token = localStorage.getItem('token');
       if (token) {
-        config.headers.Authorization = Bearer ${token};
+        config.headers.Authorization = `Bearer ${token}`;
       }
     }
     return config;
